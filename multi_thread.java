ab00bc48ede5afdd6fde259386de5d2ebe8ced7b
= <=>
2014-10-30 16:50:26 +0800

    multi thread

---
+++ multi_thread.java
@@ -0,0 +1,24 @@
+class multi_thread extends Thread
+{
+public String name;
+multi_thread(String name)
+{
+this.name=name;
+}
+
+public void run()
+{
+	for(int i=0;i<10;i++)
+	System.out.println(name+" "+i);
+}
+
+public static void main(String argv[])
+{
+	multi_thread a=new multi_thread("A");
+	multi_thread b=new multi_thread("B");
+	a.start();
+	b.start();
+
+}
+
+}
